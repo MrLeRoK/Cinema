57c628c663fabc217e111f6639cc53eb63ec679c
Алексей <Alex-vodolei@rambler.ru>
2024-05-11 20:24:55 +0400

    feat: Добавлено, компонент

--- src/App.tsx
+++ src/App.tsx
@@ -2,23 +2,40 @@
 // import {SeriesApi} from "./service-api/SeriesApi.tsx";
 
 import {MoviesApi} from "./service-api/MoviesApi.tsx";
+import {useEffect, useState} from "react";
+
+
+interface IMovies {
+    id: string;
+    alternativeTitle: string;
+    posterUrl: string;
+    rating: number;
+    title: string;
+    type: string;
+    year: number;
+
+
+}
 
 export default function App() {
+    const [movies, setMovies] = useState<IMovies[]>([])
 
-
-    const loadMovies = async () => {
-        try {
-            const moviesData = await MoviesApi();
-            console.log(moviesData);
-        } catch (error) {
-            console.error(error);
+        const loadMovies = async () => {
+            try {
+                const moviesData = await MoviesApi();
+                setMovies(moviesData)
+                console.log(moviesData);
+            } catch (error) {
+                console.error(error);
+            }
         }
-    }
-    loadMovies()
+
+        loadMovies();
+
 
     return (
 
-        <>
+        <> {movies}
             {/*<MoviesApi/>*/}
             {/*<SeriesApi/>*/}
         </>
