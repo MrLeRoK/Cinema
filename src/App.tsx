ab33cf0cf71d735c8aa077f25ff9ad9689eea747
Алексей <Alex-vodolei@rambler.ru>
2024-05-11 19:43:25 +0400

    feat: Добавлено, компонент

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
 // import {MoviesApi} from "./service-api/MoviesApi.tsx";
 // import {SeriesApi} from "./service-api/SeriesApi.tsx";
 
-
 import {MoviesApi} from "./service-api/MoviesApi.tsx";
 
 export default function App() {
 
+
     const loadMovies = async () => {
         try {
-            MoviesApi();
+            const moviesData = await MoviesApi();
+            console.log(moviesData);
         } catch (error) {
             console.error(error);
         }
