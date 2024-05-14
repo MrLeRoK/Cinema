bd26e487873f1a5435e70a629133c205c80e85a0
Алексей <Alex-vodolei@rambler.ru>
2024-05-14 23:10:04 +0400

    feat: Добавлено, компонент

--- src/components/SearchMovies/SearchMovies.tsx
+++ src/components/SearchMovies/SearchMovies.tsx
@@ -1,7 +1,25 @@
+import {ChangeEvent, useState} from "react";
+
+
+
+
+
 export const SearchMovies = () => {
+    const [loading, setLoading] = useState(false)
+    const [searchTerm, setSearchTerm] = useState('');
+
+    function handleChange(event: ChangeEvent<HTMLInputElement>) {
+        console.log(event.target.value)
+        setSearchTerm(event.target.value)
+    }
+
     return (
+
         <>
-            <input type="text"/>
+            <input type="text"  onChange={handleChange}
+
+            />
+
         </>
-    );
-};
+    )
+}
