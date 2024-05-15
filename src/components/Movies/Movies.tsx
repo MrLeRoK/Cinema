d68a3cbd28e9a763cd92b020f39b2148a67bc0a0
Алексей <Alex-vodolei@rambler.ru>
2024-05-15 15:55:50 +0400

    feat: Добавлено, разметку ListMovies

--- src/components/Movies/Movies.tsx
+++ src/components/Movies/Movies.tsx
@@ -1,7 +1,6 @@
-
-
 import {SeriesProps} from "./Movies.types.ts";
 import {ListMovies} from "../ListMovies/ListMovies.tsx";
+import style from './Movies.module.css';
 
 
 export const Movies = ({data}: SeriesProps) => {
@@ -9,8 +8,8 @@ export const Movies = ({data}: SeriesProps) => {
     console.log('movies: ', data);
 
     return (
-        <>
+        <div className={style.container}>
             <ListMovies data={data}/>
-        </>
+        </div>
     );
 };
