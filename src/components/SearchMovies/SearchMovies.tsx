7c24e2199f8b9e31b6e755926baf30d95d795ebf
Алексей <Alex-vodolei@rambler.ru>
2024-05-15 11:55:37 +0400

    feat: Добавлено, компонент

--- src/components/SearchMovies/SearchMovies.tsx
+++ src/components/SearchMovies/SearchMovies.tsx
@@ -1,25 +1,33 @@
-import {ChangeEvent, useState} from "react";
-
-
-
+import React, {useRef, useState} from "react";
 
 
 export const SearchMovies = () => {
-    const [loading, setLoading] = useState(false)
-    const [searchTerm, setSearchTerm] = useState('');
+    const input = useRef<HTMLInputElement>(null)
+    // const [loading, setLoading] = useState(false)
+    const [searchTerm, setSearchTerm] = useState(false);
 
-    function handleChange(event: ChangeEvent<HTMLInputElement>) {
-        console.log(event.target.value)
-        setSearchTerm(event.target.value)
+    // function handleChange(event: React.ChangeEvent<HTMLInputElement>) {
+    //     console.log(event.target.value);
+    //     setSearchTerm(event.target.value);
+    // }
+
+    function handleKey(event: React.KeyboardEvent<HTMLInputElement>) {
+        if (event.key === 'Enter') {
+            setSearchTerm(true)
+        }
     }
+
 
     return (
 
-        <>
-            <input type="text"  onChange={handleChange}
-
+        <div>
+            <h3 style={{color: 'white'}}>Input value: {searchTerm && input.current?.value}</h3>
+            <input type="text"
+                   ref={input}
+                   onKeyDown={handleKey}
+                   // onChange={handleChange}
             />
 
-        </>
+        </div>
     )
 }
