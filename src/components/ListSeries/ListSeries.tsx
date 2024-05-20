b6ff236e1b7bf58946836bf32552777e7e56920c
Алексей <Alex-vodolei@rambler.ru>
2024-05-20 22:24:21 +0400

    feat: Добавлено, логика в рейтинг ListSeries.tsx

--- src/components/ListSeries/ListSeries.tsx
+++ src/components/ListSeries/ListSeries.tsx
@@ -4,27 +4,33 @@ import {SeriesProps} from "../Movies/Movies.types.ts";
 // @ts-expect-error
 import style from './ListSeries.module.css'
 
-export const ListSeries = ({data} : SeriesProps) => {
+export const ListSeries = ({data}: SeriesProps) => {
 
 
     return (
         <>
-            {data.map(seriesItem => (
-                <div className={style.series_item} key={seriesItem.id}>
-                    <div className={style.series_poster_container}>
-                        <img src={seriesItem.posterUrl} alt={seriesItem.title} className={style.series_image} />
-                        <div className={style.series_info}>
-                            <div className={style.container_series_year}>
-                                <div className={style.series_year}>{seriesItem.year}</div>
-                            </div>
-                            <div className={style.container_series_rating}>
-                                <div className={style.series_rating}>{Number(seriesItem.rating).toFixed(1)}</div>
+            {data.map(seriesItem => {
+                const ratingSeries = seriesItem.rating >= 7 ? style.high_rating
+                    : seriesItem.rating >= 5 ? style.average_rating : style.low_rating;
+
+                return (
+                    <div className={style.series_item} key={seriesItem.id}>
+                        <div className={style.series_poster_container}>
+                            <img src={seriesItem.posterUrl} alt={seriesItem.title} className={style.series_image}/>
+                            <div className={style.series_info}>
+                                <div className={style.container_series_year}>
+                                    <div className={style.series_year}>{seriesItem.year}</div>
+                                </div>
+                                <div className={style.container_series_rating}>
+                                    <div className={`${style.series_rating} ${ratingSeries}`}>
+                                        {Number(seriesItem.rating).toFixed(1)}</div>
+                                </div>
                             </div>
                         </div>
+                        <div className={style.series_title}>{seriesItem.title}</div>
                     </div>
-                    <div className={style.series_title}>{seriesItem.title}</div>
-                </div>
-            ))}
+                )
+            })}
         </>
     );
 };
